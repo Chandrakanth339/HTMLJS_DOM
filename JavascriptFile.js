0268ccb9c9cbf371f929fab30ff72f230868236a
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-08-07 04:29:09 +0530

    commits

--- JavascriptFile.js
+++ JavascriptFile.js
@@ -100,7 +100,29 @@ function blurEvnt() {
 }
 
 window.addEventListener("load", () => {
-    document.getElementById("eventAttach").addEventListener("click",  () => {
+    document.getElementById("eventAttach").addEventListener("click", () => {
         console.log("Triggered Event!")
     });
 });
+
+function ajaxRequestResponse() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+            
+            // Add the JSON Parser to consider the response as a JSON object 
+            // var resultVariable = JSON.parse(xhttp.responseText);
+            // console.log(resultVariable);
+
+            // If JSON.parse is not added , then the responseText is considered as plain text and
+            // whatever is the JSON response we get, it will be considered as character stream, but not as
+            // JSON object
+            console.log(xhttp.responseText[0]); // WIll print { as this character is @ first index(0)
+        }
+        if(xhttp.status>200 && xhttp.status<400){
+            console.log("Error");
+        }
+    };
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/todos/1","true",null,null);
+    xhttp.send();
+};
